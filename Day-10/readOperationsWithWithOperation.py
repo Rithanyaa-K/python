a5688e20cc1b1461d796f955ea92db1f3175bb49
Rithanyaa-K <rithanyaa.official@gmail.com>
2026-01-08 10:31:20 +0530

    Day-10

---
+++ Day-10/readOperationsWithWithOperation.py
@@ -0,0 +1,9 @@
+with open("day-10/file.txt",'r') as f:
+    print(f.read())
+with open("day-10/file.txt",'r') as f:
+    print(f.readline())  
+with open("day-10/file.txt",'r') as f:
+    print(f.readlines()) #[]-array    and local
+
+    
+
